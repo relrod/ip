195dba7fa027d1740037220e06fbeaa30f46a10b
Ricky Elrod <ricky@elrod.me>
2015-01-25 02:51:29 -0500

    Doc comments
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Network/IP.hs
+++ src/Network/IP.hs
@@ -30,6 +30,7 @@ import Text.Parser.Char
 import Text.Parser.Combinators
 import Text.Parser.Token
 
+-- | A representation (in 'Word32' form) of an IPv4 address.
 data IPv4 = IPv4 !Word32 deriving (Eq, Ord)
 
 instance Show IPv4 where
@@ -38,9 +39,9 @@ instance Show IPv4 where
                      show ((addr `shiftR` 8) .&. 0xff) ++ "." ++
                      show (addr .&. 0xff)
 
-
 type IpParseConstraint m = (Monad m, TokenParsing m)
 
+-- | Parses an individual octet of an IPv4 IP address.
 ipv4Octet :: IpParseConstraint m => m Integer
 ipv4Octet = do
   octet <- decimal
@@ -48,6 +49,7 @@ ipv4Octet = do
     then return  octet
     else unexpected "Octet is not in range [0, 255]"
 
+-- | Parses an IPv4 address into an 'IPv4' type.
 ipv4Parser :: IpParseConstraint m => m IPv4
 ipv4Parser = do
   octet1 <- (\x -> x * (256 ^ (3 :: Integer))) <$> ipv4Octet
